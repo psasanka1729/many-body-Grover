ccfe3020c46b0c2ab0ed4863a51b482fa40f29cd
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-01-27 10:03:20 -0600

    Commit before run h_eff_energies

--- build/h_eff_energies/h_eff_energy.jl
+++ build/h_eff_energies/h_eff_energy.jl
@@ -381,7 +381,7 @@ function Eigenvalues(DELTA)
     h_eff_D = h_eff_D[3:2^L,3:2^L]; # Deleting the |0> and |xbar> basis.
     E_eff_D = eigvals(h_eff_D) # Diagonalizing H_eff matrix.
     
-    E_eff_D_sorted = sort(real(E_eff_D),rev = true); # Soring the eigenvalues in descending order.    
+    E_eff_D_sorted = sort(real(E_eff_D)); # Soring the eigenvalues in descending order.    
 
     
     return E_exact, E_eff_D_sorted
