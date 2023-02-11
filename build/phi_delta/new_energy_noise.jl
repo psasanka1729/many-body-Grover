8bb07a768beefe6b04fed09c4883ce3123f8bfa1
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-02-11 12:04:45 -0600

    Commit before run phi_delta

--- build/phi_delta/new_energy_noise.jl
+++ build/phi_delta/new_energy_noise.jl
@@ -1,11 +1,11 @@
-L = 12;
+L = 6;
 
 using Random
 using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
 using PyCall
-file = raw"12_new_Grover_gates_data.txt" # Change for every L.
+file = raw"6_new_Grover_gates_data.txt" # Change for every L.
 M = readdlm(file)
 Gates_data_1 = M[:,1];
 Gates_data_2 = M[:,2];
@@ -27,7 +27,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
 
-SEED = 16
+SEED = 6000
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
