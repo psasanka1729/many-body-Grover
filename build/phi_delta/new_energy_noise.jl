f598757cf68f07caf92e1f64d0fb69e960ed686a
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-02-20 09:38:27 -0600

    Commit before run phi_delta

--- build/phi_delta/new_energy_noise.jl
+++ build/phi_delta/new_energy_noise.jl
@@ -27,7 +27,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
 
-SEED = 10000
+SEED = 100000
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -443,9 +443,10 @@ def Write_file(Noise, Energy, Entropy):
 # delta_index runs from 0 to 63.
 delta_index = parse(Int64,ARGS[1])
 
-Delta = LinRange(0.0,0.3,64+1)
-Delta_start = Delta[delta_index+1]
-Delta_end = Delta[delta_index+2]
+Delta = LinRange(0.0,0.3,65)
+delta_start = Delta[delta_index+1]
+delta_end   = Delta[delta_index+2]
+
 Num = 10
 
 for i=0:Num
