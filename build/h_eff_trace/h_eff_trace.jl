615ba9337d205270b79bc0df85dc1d42c87fbbd0
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-02-23 11:25:11 -0600

    Commit before run h_eff_trace

--- build/h_eff_trace/h_eff_trace.jl
+++ build/h_eff_trace/h_eff_trace.jl
@@ -25,7 +25,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 6000+parse(Int64,ARGS[1])
+SEED = 10000+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -428,5 +428,5 @@ function Level_Statistics(n,Es)
 end;
 
 for i = 2:2^L-3 # relative index i.e length of the eigenvector array.
-    py"Write_file4"(i,Level_Statistics(i,E_Eff_sorted))
+    py"Write_file4"(i,Level_Statistics(i,E_eff_sorted))
 end
