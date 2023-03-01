c484728cb13b09262eb39b8fede3904a0a126e22
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-02-28 22:36:19 -0600

    cluster backup

---
+++ build/non_systematic_probability_time/non_systematic_probability_time_builder.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+import subprocess
+import numpy
+import os
+
+partition_info=['CMT',16] # = [partition,ncores]
+# partition_info=['debug',16] # = [partition,ncores]
+time_str='4-00:00:00'
+project_name=os.getcwd().split('/')[-3]
+myemail=os.environ["MYEMAIL"]
+
+#Log the current submission
+logstr='''non_systematic_probability_time:
+
+***write me***
+
+Codebase: 
+Cluster: ganymede
+gittag: '''
+
+#Do the git-ing
+cmd='git commit -a -m "Commit before run non_systematic_probability_time" > /dev/null'
+print(cmd)
+subprocess.call(cmd,shell=True)
+
+cmd='gittag.py > temp.temp'
+subprocess.call(cmd,shell=True)
+logstr+=open('temp.temp','r').readline()
+subprocess.call('rm temp.temp',shell=True)
+
+open('non_systematic_probability_time.log','w').write(logstr)
+
+#Setup the versionmap and qsub files
+vmap_file=open('versionmap.dat','w')
+vmap_file.write('vnum\tL\n')
+
+task_file=open('non_systematic_probability_time.task','w')
+template_file='non_systematic_probability_time.template'
+template_contents=open(template_file,'r').read()
+
+vnum=0
+deltas = [0.0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.2,0.21,]
+for L in xrange(16):
+	qsub_file=template_file.replace('.template','_'+str(vnum)+'.qsub')
+	fout=open(qsub_file,'w')
+
+	contents=template_contents.replace('###',str(vnum))
+        contents=contents.replace('*project*',project_name)
+	contents=contents.replace('*111*',str(deltas[L]))
+	out_file_base='data_'+str(L)+'_*lll*.out'
+	contents=contents.replace('*111*',out_file_base.replace('*lll*','julia'))
+	vmap_file.write(str(vnum)+'\t'+str(L)+'\n')
+	task_file.write('bash non_systematic_probability_time_'+str(vnum)+'.qsub\n')
+	fout.write(contents)
+	fout.close()
+	
+	vnum+=1
+	
+
+n_nodes=int(numpy.ceil(float(vnum)/partition_info[1]))
+
+# Pad to an even multiple of cores per node
+n_cores=n_nodes*partition_info[1]
+for j in range(vnum,n_cores):
+        task_file.write('echo "Fake run"\n')
+
+# Finally output sbatch file
+contents=open('non_systematic_probability_time.sbatch.template','r').read()
+contents=contents.replace('*nnn*',str(n_cores)) # The total number of processors
+contents=contents.replace('*NNN*',str(n_nodes)) # The total number of nodes
+contents=contents.replace('*ttt*',time_str) # The wall clock time per processor
+contents=contents.replace('*partition*',partition_info[0]) # Partition
+contents=contents.replace('*myemail*',myemail) # Email
+contents=contents.replace('*project*',project_name) # Project name
+open('non_systematic_probability_time.sbatch','w').write(contents)
