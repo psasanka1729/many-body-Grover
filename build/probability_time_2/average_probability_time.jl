8f84050eb0d7216f2c87456ad834a42c5f717578
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-03-12 16:02:50 -0500

    Commit before run probability_time_2

--- build/probability_time_2/average_probability_time.jl
+++ build/probability_time_2/average_probability_time.jl
@@ -26,7 +26,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 100000+parse(Int64,ARGS[1])
+SEED = 200000+parse(Int64,ARGS[1])
 Delta = 0.05
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
