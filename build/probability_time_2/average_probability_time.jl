819db2d59a889ef52131ae09dbf94815f997adcc
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-03-11 22:19:39 -0600

    Commit before run probability_time_2

--- build/probability_time_2/average_probability_time.jl
+++ build/probability_time_2/average_probability_time.jl
@@ -26,7 +26,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 200000+parse(Int64,ARGS[1])
+SEED = 100000+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -194,7 +194,7 @@ function Pxbar(full_wavefunction)
     return abs(p_xbar)^2/(2^L-1)
 end
 
-U = Grover_operator(0.06);
+U = Grover_operator(0.08);
 
 Psi_0(L) = sparse((1/sqrt(2^L))*ones(ComplexF64,2^L));
 p_0l = []
@@ -223,7 +223,7 @@ xdata = [i for i = 50:55];
 ydata = p_0l[50:55]
 
 # Define an initial guess for the parameters
-p0 = [  0.18,   0.18,   0.11, 15]
+p0 = [  0.07,   0.07,   0.18, 9.31]
 
 # Call the curve_fit function
 fit = curve_fit(model, xdata, ydata, p0)
