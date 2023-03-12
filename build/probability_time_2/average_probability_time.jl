bd76b17f6a6886f2d89db465c589acc52a99861d
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-03-12 16:22:14 -0500

    Commit before run probability_time_2

--- build/probability_time_2/average_probability_time.jl
+++ build/probability_time_2/average_probability_time.jl
@@ -26,8 +26,8 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 200000+parse(Int64,ARGS[1])
-Delta = 0.1
+SEED = 100000+parse(Int64,ARGS[1])
+Delta = 0.12
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -224,7 +224,7 @@ xdata = [i for i = 50:60];
 ydata = p_0l[50:60]
 
 # Define an initial guess for the parameters
-p0 = [  0.30,   0.30,   0.39, -3.37]
+p0 = [  0.24,   0.24,   0.43, -6.54]
 
 # Call the curve_fit function
 fit = curve_fit(model, xdata, ydata, p0)
