bf9fbf8ea8a5b06f20c3401b2fb18d6957096ec9
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-03-13 11:03:38 -0500

    Commit before run phi_delta

--- build/phi_delta/energy_noise.jl
+++ build/phi_delta/energy_noise.jl
@@ -1,4 +1,4 @@
-L = 12;
+L = 14;
 
 using NPZ
 using Random
@@ -6,7 +6,7 @@ using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
 using PyCall
-file = raw"12_new_Grover_gates_data.txt" # Change for every L.
+file = raw"14_new_Grover_gates_data.txt" # Change for every L.
 M = readdlm(file)
 Gates_data_1 = M[:,1];
 Gates_data_2 = M[:,2];
@@ -367,7 +367,7 @@ delta_index = parse(Int64,ARGS[1])
 Delta = LinRange(0.0,0.18,64+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
-Num = 1
+Num = 4
 
               
 #=
