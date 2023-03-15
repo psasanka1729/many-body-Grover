7e635dbc85de8c02d3556b47718d63d269c094b2
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-03-15 00:36:41 -0500

    Commit before run KLd_10_run_1

--- build/KLd_10_run_1/KL_divergence_h_eff.jl
+++ build/KLd_10_run_1/KL_divergence_h_eff.jl
@@ -26,7 +26,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 50000+parse(Int64,ARGS[1])
+SEED = 40000+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -437,7 +437,6 @@ function Eigenvectors(DELTA)
     return h_eff_eigenvectors
 end;
 
-H_eff_Eigvecs = Eigenvectors(0.0);
 
 function KLd(Eigenvectors_Matrix)
     KL = []
--- build/KLd_10_run_2/KL_divergence_h_eff.jl
+++ build/KLd_10_run_2/KL_divergence_h_eff.jl
@@ -352,7 +352,6 @@ function Eigenvectors(DELTA)
     return h_eff_eigenvectors
 end;
 
-H_eff_Eigvecs = Eigenvectors(0.0);
 
 function KLd(Eigenvectors_Matrix)
     KL = []
