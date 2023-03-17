f055a90a6fc0454247cbc49006f0e4df4c3d1e8f
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-03-16 23:53:49 -0500

    Commit before run phi_delta

--- build/phi_delta/energy_noise.jl
+++ build/phi_delta/energy_noise.jl
@@ -361,13 +361,13 @@ end;
 #    f.write(str(Noise) +'\t'+ str(Energy)+ '\t' + str(Entropy) +'\n')
 #"""
 
-# delta_index runs from 0 to 63.
+# delta_index runs from 0 to 127.
 delta_index = parse(Int64,ARGS[1])
 
-Delta = LinRange(0.0,0.18,64+1)
+Delta = LinRange(0.0,0.18,128+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
-Num = 4
+Num = 1
 
               
 #=
