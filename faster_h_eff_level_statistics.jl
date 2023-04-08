230b130ff6e3898a978290679f90fd0bac50915e
Sasanka <31107620+psasanka1729@users.noreply.github.com>
2023-04-08 18:12:24 -0500

    made faster using sparse matrices

--- faster_h_eff_level_statistics.jl
+++ faster_h_eff_level_statistics.jl
@@ -1,9 +1,11 @@
-L = 8;
-
+using NPZ
 using Random
 using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
+
+L = 10;
+
 file = raw""*string(L)*"_new_Grover_gates_data.txt" # Change for every L.
 M = readdlm(file)
 Gates_data_1 = M[:,1];
@@ -25,18 +27,19 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 50000
+SEED = 1945
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
-I2 = [1 0; 0 1];
-Z = [1 0;0 -1];
-H = (1/sqrt(2))*[1 1;1 -1]
-Rx(theta)= exp(-1im*(theta/2)*([1 0;0 1]-[0 1;1 0]));
-Hadamard(noise) = exp(-1im*(pi/2+noise)*(I2-H)) #Ry(pi/2+noise)*Pauli_Z;
-CX(noise) = exp(-1im*((pi/2+noise))*([1 0;0 1]-[0 1;1 0])); # This is X gate.
-Z_gate(noise) = exp(-1im*(pi/2+noise)*(I2-Z)) #Hadamard(noise)*CX(noise)*Hadamard(noise); # noise
-Identity(dimension) = 1* Matrix(I, dimension, dimension);
+I2 = sparse([1 0; 0 1]);
+Z  = sparse([1 0;0 -1]);
+X  = sparse([0 1;1 0])
+H  = (1/sqrt(2))*[1 1;1 -1]
+Rx(theta)       = sparse(exp(-1im*(theta/2)*collect(I2-X)));
+Hadamard(noise) = sparse(exp(-1im*(pi/2+noise)*collect(I2-H))) #Ry(pi/2+noise)*Pauli_Z;
+CX(noise)       = sparse(exp(-1im*((pi/2+noise))*collect(I2-X))); # This is X gate.
+Z_gate(noise)   = sparse(exp(-1im*(pi/2+noise)*collect(I2-Z))) #Hadamard(noise)*CX(noise)*Hadamard(noise); # noise
+Identity(dimension) = spdiagm(0 => ones(dimension));
 int(x) = floor(Int,x);
 
 function Matrix_Gate(Gate, Qubit) # Previously known as multi qubit gate.
@@ -48,12 +51,12 @@ function Matrix_Gate(Gate, Qubit) # Previously known as multi qubit gate.
         
         M = sparse(Gate)
         for i=2:L
-            M = kron(M, sparse([1 0;0 1]))
+            M = kron(M, I2)
         end
         
     else
         
-        M = sparse([1 0;0 1])
+        M = I2
         for i=2:L
             if i == Qubit
                 M = kron(M, Gate)
@@ -67,9 +70,6 @@ function Matrix_Gate(Gate, Qubit) # Previously known as multi qubit gate.
 end;
 
 function CU(U,c,t)
-    
-    I2 = sparse([1 0;0 1])
-    Z = sparse([1 0;0 -1])
 
     PI_0 = (I2+Z)/2
     PI_1 = (I2-Z)/2
@@ -104,70 +104,12 @@ function CU(U,c,t)
     return sparse(PI_0_matrix + PI_1_matrix)     
 end;
 
-using PyCall
-py"""
-import numpy
-import numpy.linalg
-def adjoint(psi):
-    return psi.conjugate().transpose()
-def psi_to_rho(psi):
-    return numpy.outer(psi,psi.conjugate())
-def exp_val(psi, op):
-    return numpy.real(numpy.dot(adjoint(psi),op.dot(psi)))
-def norm_sq(psi):
-    return numpy.real(numpy.dot(adjoint(psi),psi))
-def normalize(psi,tol=1e-9):
-    ns=norm_sq(psi)**0.5
-    if ns < tol:
-        raise ValueError
-    return psi/ns
-def is_herm(M,tol=1e-9):
-    if M.shape[0]!=M.shape[1]:
-        return False
-    diff=M-adjoint(M)
-    return max(numpy.abs(diff.flatten())) < tol
-def is_unitary(M,tol=1e-9):
-    if M.shape[0]!=M.shape[1]:
-        return False
-    diff=M.dot(adjoint(M))-numpy.identity((M.shape[0]))
-    return max(numpy.abs(diff.flatten())) < tol
-def eigu(U,tol=1e-9):
-    (E_1,V_1)=numpy.linalg.eigh(U+adjoint(U))
-    U_1=adjoint(V_1).dot(U).dot(V_1)
-    H_1=adjoint(V_1).dot(U+adjoint(U)).dot(V_1)
-    non_diag_lst=[]
-    j=0
-    while j < U_1.shape[0]:
-        k=0
-        while k < U_1.shape[0]:
-            if j!=k and abs(U_1[j,k]) > tol:
-                if j not in non_diag_lst:
-                    non_diag_lst.append(j)
-                if k not in non_diag_lst:
-                    non_diag_lst.append(k)
-            k+=1
-        j+=1
-    if len(non_diag_lst) > 0:
-        non_diag_lst=numpy.sort(numpy.array(non_diag_lst))
-        U_1_cut=U_1[non_diag_lst,:][:,non_diag_lst]
-        (E_2_cut,V_2_cut)=numpy.linalg.eigh(1.j*(U_1_cut-adjoint(U_1_cut)))
-        V_2=numpy.identity((U.shape[0]),dtype=V_2_cut.dtype)
-        for j in range(len(non_diag_lst)):
-            V_2[non_diag_lst[j],non_diag_lst]=V_2_cut[j,:]
-        V_1=V_1.dot(V_2)
-        U_1=adjoint(V_2).dot(U_1).dot(V_2)
-    # Sort by phase
-    U_1=numpy.diag(U_1)
-    inds=numpy.argsort(numpy.imag(numpy.log(U_1)))
-    return (U_1[inds],V_1[:,inds]) # = (U_d,V) s.t. U=V*U_d*V^\dagger
-"""
-
 U_0 = Identity(2^L)#[-1 0 0 0; 0 1 0 0; 0 0 1 0;0 0 0 1];
 U_0[1,1] = -1
 A = ones(2^L,2^L);
 U_x = (2/2^L)*A-Identity(2^L); # 2\s><s|-I
-G_exact = U_x*U_0
-V = py"eigu"(G_exact)[2];
+G_exact = U_x*U_0;
+#V = py"eigu"(G_exact)[2];
 
 #DELTA = 0.01
 function H_eff_Eigenvalues(DELTA)
@@ -345,12 +287,12 @@ function H_eff_Eigenvalues(DELTA)
             elseif Gates_data_new_1[k] == "X"
 
                 Qubit = Gates_data_new_3[k] # qubit.
-                H_k = Matrix_Gate([1 0;0 1]-[0 1;1 0],Qubit) #= H_X = I2-X. =#
+                H_k = Matrix_Gate(I2-X,Qubit) #= H_X = I2-X. =#
             
             elseif Gates_data_new_1[k] == "Z"
 
                 Qubit = Gates_data_new_3[k] # qubit.
-                H_k = Matrix_Gate([1 0;0 1]-[1 0;0 -1],Qubit) #= H_Z = I2-Z. =#
+                H_k = Matrix_Gate(I2-Z,Qubit) #= H_Z = I2-Z. =#
             
             else
         
@@ -358,7 +300,7 @@ function H_eff_Eigenvalues(DELTA)
                 Control_Qubit = int(Gates_data_new_2[k])
                 Target_Qubit = int(Gates_data_new_3[k])
                 #= H = ((I-Z)/2)_c \otimes ((I-X)/2)_t.=#
-                Matrices = Dict("I" => [1 0;0 1],"U" => [1 -1;-1 1]/2, "PI_1" => (I2-Z)/2)
+                Matrices = Dict("I" => I2,"U" => [1 -1;-1 1]/2, "PI_1" => (I2-Z)/2)
                 p1 = fill("I", L)
                 p1[Control_Qubit] = "PI_1"
                 p1[Target_Qubit] = "U"
@@ -373,8 +315,8 @@ function H_eff_Eigenvalues(DELTA)
     end; 
     
     #= The following loop sums over all epsilon to get H_eff. =#
-    h_eff = zeros(2^L,2^L);
-    for i = 1:length(U_list)
+    h_eff = sparse(zeros(2^L,2^L));
+    @time for i = 1:length(U_list)
         h_eff += NOISE_list[i]*kth_term(i)
     end        
 
@@ -394,27 +336,17 @@ function H_eff_Eigenvalues(DELTA)
     =#
     h_eff_trunc = (Identity(2^L)-P_xbar)*(Identity(2^L)-P_0)*h_eff*(Identity(2^L)-P_0)*(Identity(2^L)-P_xbar)    
     
-    
-    
 
     h_eff_D = h_eff_trunc[3:2^L,3:2^L]; # Deleting the |0> and |xbar> basis.
-    E_eff_D = eigvals(h_eff_D) # Diagonalizing H_eff matrix.
+    @time E_eff_D = eigvals(h_eff_D) # Diagonalizing H_eff matrix.
     
     E_eff_D_sorted = sort(real(E_eff_D),rev = true); # Soring the eigenvalues in descending order.    
 
     
     return E_eff_D_sorted
-    #return GROVER_DELTA
 end;
 
-@time Eigenvalues(0.0)
-
-E_eff_sorted = H_eff_Eigenvalues(0.0);
-
-using NPZ
-
-#npzwrite("trace_h_squared.npy",real(tr(h_eff_matrix*h_eff_matrix)))
-
+H_eff_Eigvals = H_eff_Eigenvalues(0.0);
 
 npzwrite("h_eff_eigenvalues.npy",E_eff_sorted)
 
