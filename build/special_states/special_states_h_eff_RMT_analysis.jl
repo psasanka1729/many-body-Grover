948843c2b5d8abd46aa2e96f0c34a843590770e4
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-05-02 09:32:14 -0500

    Commit before run special_states

--- build/special_states/special_states_h_eff_RMT_analysis.jl
+++ build/special_states/special_states_h_eff_RMT_analysis.jl
@@ -28,8 +28,8 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-DELTA = 0.14
-SEED = 140000+parse(Int64,ARGS[1])
+DELTA = 0.16
+SEED = 160000+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
