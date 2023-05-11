9de495d6c6da23405b6f162dc70470ea9d76148b
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-05-11 10:20:49 -0500

    Commit before run KLd_10

--- build/KLd_10/faster_h_eff_level_statistics.jl
+++ build/KLd_10/faster_h_eff_level_statistics.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
 
-L = 12;
+L = 14;
 
 file = raw""*string(L)*"_new_Grover_gates_data.txt" # Change for every L.
 M = readdlm(file)
@@ -28,7 +28,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 8256+parse(Int64,ARGS[1])
+SEED = 600000+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -417,7 +417,7 @@ level_statistics_file = open("level_statistics.txt", "w")
 KLd_file              = open("KLd.txt", "w");
 
 h_eff_matrix_created_once = h_eff_from_derivative(1.e-8)
-
+#=
 bulk_energies = h_eff_bulk_energies(h_eff_matrix_created_once)
 
 for i = 1:2^L-2
@@ -449,6 +449,7 @@ end
 
 # Close the file
 close(level_statistics_file)
+=#
 
 h_eff_eigenvectors = eigvecs(h_eff_matrix_created_once)
 
