5a866d342b9b4b2044b23d0452ab9bbc5b0fc1ad
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-05-17 10:10:13 -0500

    Commit before run h_eff_spectrum

--- build/h_eff_spectrum/faster_h_eff_level_statistics.jl
+++ build/h_eff_spectrum/faster_h_eff_level_statistics.jl
@@ -28,7 +28,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 1192+parse(Int64,ARGS[1])
+SEED = 1256+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
