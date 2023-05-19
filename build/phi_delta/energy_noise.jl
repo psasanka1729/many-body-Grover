044f591139a143c4b8ea1bec6bd89c105a2aa129
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-05-19 16:42:38 -0500

    Commit before run phi_delta

--- build/phi_delta/energy_noise.jl
+++ build/phi_delta/energy_noise.jl
@@ -1,4 +1,4 @@
-L = 10;
+L = 12;
 
 using Random
 using LinearAlgebra
@@ -547,7 +547,7 @@ def Write_file(Noise, Energy, Entropy):
 # delta_index runs from 0 to 128.
 delta_index = parse(Int64,ARGS[1])
 
-Delta = LinRange(0.0,0.25,32+1)
+Delta = LinRange(0.0,0.5,128+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
 Num = 10
