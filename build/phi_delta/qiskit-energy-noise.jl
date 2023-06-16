ab2657447438b85e369194bfc77a43de854aaa05
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-06-15 22:53:35 -0500

    Commit before run phi_delta

--- build/phi_delta/qiskit-energy-noise.jl
+++ build/phi_delta/qiskit-energy-noise.jl
@@ -1,11 +1,11 @@
-L = 10;
+L = 8;
 
 using PyCall
 using Random
 using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
-file = raw"gates_list_10.txt" # Change for every L.
+file = raw"gates_list_8.txt" # Change for every L.
 M = readdlm(file)
 # Name.
 Gates_data_1 = M[:,1];
@@ -18,7 +18,7 @@ Number_of_Gates = length(Gates_data_1)
 
 Gates_data_2;
 
-SEED = 1945
+SEED = 32000
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -328,10 +328,10 @@ Entropies = []
 # delta_index runs from 0 to 128.
 delta_index = parse(Int64,ARGS[1])
 #
-Delta = LinRange(0.0,0.1,64+1)
+Delta = LinRange(0.0,0.2,64+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
-Num = 5
+Num = 30
 
 for i=0:Num
     delta = delta_start+(i/Num)*(delta_end-delta_start)
