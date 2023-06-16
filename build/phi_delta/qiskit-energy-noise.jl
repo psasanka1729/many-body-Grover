f761b143ee046c27ff9de0c0b45451a9d3c82308
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-06-16 09:45:02 -0500

    Commit before run phi_delta

--- build/phi_delta/qiskit-energy-noise.jl
+++ build/phi_delta/qiskit-energy-noise.jl
@@ -18,7 +18,7 @@ Number_of_Gates = length(Gates_data_1)
 
 Gates_data_2;
 
-SEED = 32000
+SEED = 5000
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -333,7 +333,7 @@ delta_index = parse(Int64,ARGS[1])
 Delta = LinRange(0.0,0.1,64+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
-Num = 10
+Num = 5
 
 for i=0:Num
     delta = delta_start+(i/Num)*(delta_end-delta_start)
