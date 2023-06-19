579f9b443fbc1d32c24baeca6710e6e098fe66dd
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-06-18 23:26:26 -0500

    Commit before run phi_delta

--- build/phi_delta/qiskit-energy-noise.jl
+++ build/phi_delta/qiskit-energy-noise.jl
@@ -1,4 +1,4 @@
-L = 10;
+L = 8;
 
 using PyCall
 using Random
@@ -17,7 +17,7 @@ Gates_data_3 = M[:,3];
 Number_of_Gates = length(Gates_data_1)
 
 
-SEED = 5000
+SEED = 50000
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -331,10 +331,10 @@ Entropies = []
 # delta_index runs from 0 to 128.
 delta_index = parse(Int64,ARGS[1])
 #
-Delta = LinRange(0.0,0.5,64+1)
+Delta = LinRange(0.0,0.4,64+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
-Num = 10
+Num = 15
 
 for i=0:Num
     delta = delta_start+(i/Num)*(delta_end-delta_start)
