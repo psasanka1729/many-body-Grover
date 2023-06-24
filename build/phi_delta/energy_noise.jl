bf584bdd4b0c7fdab6b5c4737c86fa1b15aa944d
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-06-24 09:47:30 -0500

    Commit before run phi_delta

--- build/phi_delta/energy_noise.jl
+++ build/phi_delta/energy_noise.jl
@@ -28,7 +28,7 @@ Number_of_Gates = U_0_gate_number+U_x_gate_number
 
 
 # Good seeds = 10,14, 1945, 1337, 141421, 1414, 173205075, 1642, 1942.
-SEED = 1786
+SEED = 1942
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
