fb95493ee19ca9a2756fef75e5681d71f37fc994
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-08-23 13:52:26 -0500

    Commit before run special_states

--- build/special_states/special_states_h_eff_RMT_analysis.jl
+++ build/special_states/special_states_h_eff_RMT_analysis.jl
@@ -283,7 +283,7 @@ function sigma_y_to_sigma_z_basis_change(Matrix)
 end;
 =#
 # Changing the H_spec matrix from sigma_y basis to sigma_z basis.
-H_EFF_MATRIX = h_eff_special_states(0.1,1.e-8)
+H_EFF_MATRIX = h_eff_special_states(0.2,1.e-8)
 h_spec_z_basis = H_EFF_MATRIX
 #h_spec_z_basis           = sigma_y_to_sigma_z_basis_change(h_spec_y_basis);
 
