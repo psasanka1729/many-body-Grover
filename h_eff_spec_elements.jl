32e5713c9e0a7bcc5fa1470a08ffcfbf45a6374e
Sasanka <31107620+psasanka1729@users.noreply.github.com>
2023-08-29 15:49:17 -0500

    h_spec elements to determine condition on delta

---
+++ h_eff_spec_elements.jl
@@ -0,0 +1,520 @@
+L = 8;
+
+using PyCall
+using Random
+using LinearAlgebra
+using SparseArrays
+using DelimitedFiles
+using Statistics
+file = raw""*string(L)*"_new_Grover_gates_data.txt" # Change for every L.
+M = readdlm(file)
+Gates_data_1 = M[:,1];
+Gates_data_2 = M[:,2];
+Gates_data_3 = M[:,3];
+
+U_0_gate_number =  (L            # L X gate on left of MCX
+                  + 1            # H gate on left of MCX
+                  + 2*L^2-6*L+5  # MCX gate
+                  + 1            # H gate on right of MCX
+                  + L)           # L X gate on right of MCX
+
+U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
+                  + L-1          # L-1 X gate on left of MCX
+                  + 1            # Z gate on left of MCX
+                  + 2*L^2-6*L+5  # MCX gate
+                  + 1            # Z gate on right of MCX
+                  + L-1          # L-1 H gate on right of MCX   
+                  + L-1)          # L-1 X gate on right of MCX)             
+Number_of_Gates = U_0_gate_number+U_x_gate_number
+
+SEED = 4000+parse(Int64,ARGS[1])
+Random.seed!(SEED)
+NOISE = 2*rand(Float64,Number_of_Gates).-1;
+
+I2 = sparse([1 0; 0 1]);
+Pauli_Z  = sparse([1 0;0 -1]);
+Pauli_X  = sparse([0 1;1 0])
+H  = (1/sqrt(2))*[1 1;1 -1]
+Rx(theta)       = sparse(exp(-1im*(theta/2)*collect(I2-Pauli_X)));
+Hadamard(noise) = sparse(exp(-1im*(pi/2+noise)*collect(I2-H))) #Ry(pi/2+noise)*Pauli_Z;
+X(noise)        = sparse(exp(-1im*((pi/2+noise))*collect(I2-Pauli_X))); # This is X gate.
+Z(noise)        = sparse(exp(-1im*(pi/2+noise)*collect(I2-Pauli_Z))) #Hadamard(noise)*CX(noise)*Hadamard(noise); # noise
+Identity(dimension) = spdiagm(0 => ones(dimension));
+int(x) = floor(Int,x);
+
+#=
+function single_qubit_gate_matrix(single_qubit_gate, qubit)
+    
+    ## The case Qubit=1 is treated differently because we need to
+    # initialize the matrix as U before starting the kronecker product.
+    
+    if qubit == 1
+        
+        gate_matrix = sparse(single_qubit_gate)
+        for i=2:L
+            gate_matrix = kron(gate_matrix, I2)
+        end
+        
+    #=
+        Single qubit gates acting on qubits othe than the first.
+        =#
+    else
+        
+        gate_matrix = I2
+        for i=2:L
+            if i == qubit
+                gate_matrix = kron(gate_matrix, single_qubit_gate)
+            else
+                gate_matrix = kron(gate_matrix, I2)
+            end
+        end
+    end
+    
+    return gate_matrix
+end;
+=#
+
+function single_qubit_gate_matrix(Gate, Qubit) # Previously known as multi qubit gate.
+    
+    ## The case Qubit=1 is treated differently because we need to
+    # initialize the matrix as U before starting the kronecker product.
+    
+    if Qubit == 1
+        
+        M = sparse(Gate)
+        for i=2:L
+            M = kron(M, sparse([1 0;0 1]))
+        end
+        
+    else
+        
+        M = sparse([1 0;0 1])
+        for i=2:L
+            if i == Qubit
+                M = kron(M, Gate)
+            else
+                M = kron(M, sparse([1 0;0 1]))
+            end
+        end
+    end
+    
+    return M
+end;
+
+#=
+function single_qubit_controlled_gate_matrix(single_qubit_gate,c,t)
+
+    # |0><0|.
+    PI_0 = (I2+Z)/2
+    # |1><1|.
+    PI_1 = (I2-Z)/2
+     
+    Matrices = Dict("I" => I2,"PI_0" => PI_0,"U" => single_qubit_gate, "PI_1" => PI_1)
+    
+    p0 = fill("I", L)
+    p1 = fill("I", L)
+    
+    p0[c] = "PI_0"
+    p1[c] = "PI_1"
+    p1[t] = "U"
+
+    
+    PI_0_matrix = Matrices[p0[1]]
+    for i = 2:L
+        PI_0_matrix = kron(PI_0_matrix,Matrices[p0[i]])
+    end        
+        
+    PI_1_matrix = Matrices[p1[1]]   
+    for i = 2:L
+        PI_1_matrix = kron(PI_1_matrix,Matrices[p1[i]])        
+    end
+           
+    return sparse(PI_0_matrix + PI_1_matrix)     
+end;
+=#
+function single_qubit_controlled_gate_matrix(U,c,t)
+    
+    I2 = sparse([1 0;0 1])
+    Z = sparse([1 0;0 -1])
+
+    PI_0 = (I2+Z)/2
+    PI_1 = (I2-Z)/2
+     
+    #function Rx(Noise)
+        #A = cos((pi+Noise)/2)
+        #B = -1im*sin((pi+Noise)/2)
+        #return 1im*[A B;B A]
+    #end
+    
+    Matrices = Dict("I" => I2,"PI_0" => PI_0,"U" => U, "PI_1" => PI_1)
+    
+    p0 = fill("I", L)
+    p1 = fill("I", L)
+    
+    p0[c] = "PI_0"
+    p1[c] = "PI_1"
+    p1[t] = "U"
+
+    
+    PI_0_matrix = Matrices[p0[1]]
+    for i = 2:L
+        PI_0_matrix = kron(PI_0_matrix,Matrices[p0[i]])
+    end        
+        
+    PI_1_matrix = Matrices[p1[1]]   
+    for i = 2:L
+        PI_1_matrix = kron(PI_1_matrix,Matrices[p1[i]])        
+    end
+           
+    #return p0,p1
+    return PI_0_matrix + PI_1_matrix     
+end;
+
+using PyCall
+py"""
+import numpy
+import numpy.linalg
+def adjoint(psi):
+    return psi.conjugate().transpose()
+def psi_to_rho(psi):
+    return numpy.outer(psi,psi.conjugate())
+def exp_val(psi, op):
+    return numpy.real(numpy.dot(adjoint(psi),op.dot(psi)))
+def norm_sq(psi):
+    return numpy.real(numpy.dot(adjoint(psi),psi))
+def normalize(psi,tol=1e-9):
+    ns=norm_sq(psi)**0.5
+    if ns < tol:
+        raise ValueError
+    return psi/ns
+def is_herm(M,tol=1e-9):
+    if M.shape[0]!=M.shape[1]:
+        return False
+    diff=M-adjoint(M)
+    return max(numpy.abs(diff.flatten())) < tol
+def is_unitary(M,tol=1e-9):
+    if M.shape[0]!=M.shape[1]:
+        return False
+    diff=M.dot(adjoint(M))-numpy.identity((M.shape[0]))
+    return max(numpy.abs(diff.flatten())) < tol
+def eigu(U,tol=1e-9):
+    (E_1,V_1)=numpy.linalg.eigh(U+adjoint(U))
+    U_1=adjoint(V_1).dot(U).dot(V_1)
+    H_1=adjoint(V_1).dot(U+adjoint(U)).dot(V_1)
+    non_diag_lst=[]
+    j=0
+    while j < U_1.shape[0]:
+        k=0
+        while k < U_1.shape[0]:
+            if j!=k and abs(U_1[j,k]) > tol:
+                if j not in non_diag_lst:
+                    non_diag_lst.append(j)
+                if k not in non_diag_lst:
+                    non_diag_lst.append(k)
+            k+=1
+        j+=1
+    if len(non_diag_lst) > 0:
+        non_diag_lst=numpy.sort(numpy.array(non_diag_lst))
+        U_1_cut=U_1[non_diag_lst,:][:,non_diag_lst]
+        (E_2_cut,V_2_cut)=numpy.linalg.eigh(1.j*(U_1_cut-adjoint(U_1_cut)))
+        V_2=numpy.identity((U.shape[0]),dtype=V_2_cut.dtype)
+        for j in range(len(non_diag_lst)):
+            V_2[non_diag_lst[j],non_diag_lst]=V_2_cut[j,:]
+        V_1=V_1.dot(V_2)
+        U_1=adjoint(V_2).dot(U_1).dot(V_2)
+    # Sort by phase
+    U_1=numpy.diag(U_1)
+    inds=numpy.argsort(numpy.imag(numpy.log(U_1)))
+    return (U_1[inds],V_1[:,inds]) # = (U_d,V) s.t. U=V*U_d*V^\dagger
+"""
+
+U_0 = Identity(2^L)#[-1 0 0 0; 0 1 0 0; 0 0 1 0;0 0 0 1];
+U_0[1,1] = -1
+A = ones(2^L,2^L);
+U_x = (2/2^L)*A-Identity(2^L); # 2\s><s|-I
+G_exact = U_x*U_0;
+#V = py"eigu"(G_exact)[2];
+
+function x_bar(n)
+    s = zeros(2^L)
+    k_n = (2*pi*n)/(2^L-1)
+    for j=1:2^L-1
+        ket_j    = zeros(2^L);
+        ket_j[j+1] = 1 
+        s = s+ exp(1im*(j-1)*k_n)*ket_j
+    end
+    return s/sqrt(2^L-1)
+end;
+
+function sigma_z_to_x_bar_basis_change_matrix(L)
+    V     = spzeros(2^L,2^L)
+    ket_0 = spzeros(2^L)
+    ket_0[1] = 1
+    ket_1    = spzeros(2^L);
+    ket_1[2] = 1
+    ket_xbar = x_bar(1)
+    eigenstate_1 = (ket_0-1im*ket_xbar)/sqrt(2)
+    eigenstate_2 = (ket_0+1im*ket_xbar)/sqrt(2)
+    V = V+ ket_1*(eigenstate_1')
+    V = V+ ket_0*(eigenstate_2')
+    
+    # The buk.
+    for n=2:2^L-1
+        # ket_n has n+1 th position as 1 in comutational basis.
+        ket_n    = spzeros(2^L);
+        ket_n[n+1] = 1 
+        
+        V = V+ket_n*(x_bar(n-1)')
+    end
+    return V
+end;
+
+basis_change_matrix = sigma_z_to_x_bar_basis_change_matrix(L);
+
+function grover_effective_Hamiltonian_matrix(DELTA)
+    
+    U_list = [];
+    U_x_delta = sparse(Identity(2^L));
+    #ux_list = []
+    NOISE_list = []
+
+    Gates_data_new_1 = []
+    Gates_data_new_2 = []
+    Gates_data_new_3 = []
+    
+    # U_x
+    for i = U_0_gate_number+1: U_0_gate_number+U_x_gate_number
+        if Gates_data_1[i] == "H"
+            
+            
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)
+            h_matrix = single_qubit_gate_matrix(Hadamard(DELTA*epsilon), Gates_data_3[i])
+            U_x_delta *= h_matrix
+        
+            push!(Gates_data_new_1,"H")
+            push!(Gates_data_new_2,0.0)
+            push!(Gates_data_new_3,Gates_data_3[i])
+        
+        
+            push!(U_list,single_qubit_gate_matrix(Hadamard(0.0), Gates_data_3[i])) # Noiseless.
+            
+        elseif Gates_data_1[i] == "X"
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            x_matrix = single_qubit_gate_matrix(X(DELTA*epsilon),Gates_data_3[i])
+            U_x_delta *= x_matrix
+        
+            push!(Gates_data_new_1,"X")
+            push!(Gates_data_new_2,0.0)
+            push!(Gates_data_new_3,Gates_data_3[i]) 
+        
+        
+            push!(U_list,single_qubit_gate_matrix(X(0.0),Gates_data_3[i])) # Noiseless.
+            
+        elseif Gates_data_1[i] == "Z"
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            z_matrix = single_qubit_gate_matrix(Z_gate(DELTA*epsilon),Gates_data_3[i])
+            U_x_delta *= z_matrix
+        
+            push!(Gates_data_new_1,"Z")
+            push!(Gates_data_new_2,0.0)
+            push!(Gates_data_new_3,Gates_data_3[i]) 
+        
+        
+            push!(U_list,single_qubit_gate_matrix(Z_gate(0.0),Gates_data_3[i])) # Noiseless.
+            
+        else
+            #push!(ux_list,"CRX")
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            rx_matrix = single_qubit_controlled_gate_matrix(Rx(Gates_data_1[i]+DELTA*epsilon), Gates_data_2[i], Gates_data_3[i])
+            U_x_delta *= rx_matrix
+        
+            push!(Gates_data_new_1,Gates_data_1[i])
+            push!(Gates_data_new_2,Gates_data_2[i])
+            push!(Gates_data_new_3,Gates_data_3[i])
+        
+        
+            push!(U_list,single_qubit_controlled_gate_matrix(Rx(Gates_data_1[i]), Gates_data_2[i], Gates_data_3[i])) # Noiselss.
+            
+        end
+    end
+    
+    U_0_delta = sparse(Identity(2^L));
+    
+    #u0_list = []
+    # U_0
+    for i = 1 : U_0_gate_number
+        if Gates_data_1[i] == "H"
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            h_matrix = single_qubit_gate_matrix(Hadamard(DELTA*epsilon), Gates_data_3[i])
+            U_0_delta *= h_matrix
+        
+            push!(Gates_data_new_1,"H")
+            push!(Gates_data_new_2,0.0)
+            push!(Gates_data_new_3,Gates_data_3[i])
+        
+        
+            push!(U_list,single_qubit_gate_matrix(Hadamard(0.0), Gates_data_3[i])) # Noiseless.
+            
+        elseif Gates_data_1[i] == "X"
+
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            x_matrix = single_qubit_gate_matrix(X(DELTA*epsilon),Gates_data_3[i])
+            U_0_delta *= x_matrix
+        
+            push!(Gates_data_new_1,"X")
+            push!(Gates_data_new_2,0.0)
+            push!(Gates_data_new_3,Gates_data_3[i]) 
+        
+        
+            push!(U_list,single_qubit_gate_matrix(X(0.0),Gates_data_3[i])) # Noiseless.
+            
+        elseif Gates_data_1[i] == "Z"
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            z_matrix = single_qubit_gate_matrix(Z_gate(DELTA*epsilon),Gates_data_3[i])
+            U_x_delta *= z_matrix
+        
+            push!(Gates_data_new_1,"Z")
+            push!(Gates_data_new_2,0.0)
+            push!(Gates_data_new_3,Gates_data_3[i]) 
+        
+        
+            push!(U_list,single_qubit_gate_matrix(Z_gate(0.0),Gates_data_3[i])) # Noiseless.
+            
+        else
+            #push!(u0_list,"CRX")
+        
+            epsilon = NOISE[i]
+            push!(NOISE_list,epsilon)        
+            rx_matrix = single_qubit_controlled_gate_matrix(Rx(Gates_data_1[i]+DELTA*epsilon), Gates_data_2[i], Gates_data_3[i])
+            U_0_delta *= rx_matrix
+        
+            push!(Gates_data_new_1,Gates_data_1[i])
+            push!(Gates_data_new_2,Gates_data_2[i])
+            push!(Gates_data_new_3,Gates_data_3[i])
+        
+        
+            push!(U_list,single_qubit_controlled_gate_matrix(Rx(Gates_data_1[i]), Gates_data_2[i], Gates_data_3[i])) # Noiseless.
+            
+        end
+    end
+        
+    GROVER_DELTA = U_x_delta*U_0_delta
+    
+    function kth_term(k)
+
+            f_k = Identity(2^L);
+    
+            for i = k:length(U_list)-1
+                f_k = f_k*collect(U_list[length(U_list)-i+k])
+            end     
+            #= Corresponding H for the kth term. =#
+            if Gates_data_new_1[k] == "H"
+
+                Qubit = Gates_data_new_3[k] # qubit.
+                H_k = single_qubit_gate_matrix(I2-H,Qubit) #= H_had = I2-Had. =#
+
+            elseif Gates_data_new_1[k] == "X"
+
+                Qubit = Gates_data_new_3[k] # qubit.
+                H_k = single_qubit_gate_matrix([1 0;0 1]-[0 1;1 0],Qubit) #= H_X = I2-X. =#
+            
+            elseif Gates_data_new_1[k] == "Z"
+
+                Qubit = Gates_data_new_3[k] # qubit.
+                H_k = single_qubit_gate_matrix([1 0;0 1]-[1 0;0 -1],Qubit) #= H_Z = I2-Z. =#
+            
+            else
+        
+                Angle = Gates_data_new_1[k]
+                Control_Qubit = int(Gates_data_new_2[k])
+                Target_Qubit = int(Gates_data_new_3[k])
+                #= H = ((I-Z)/2)_c \otimes ((I-X)/2)_t.=#
+                Matrices = Dict("I" => [1 0;0 1],"U" => [1 -1;-1 1]/2, "PI_1" => (I2-Pauli_Z)/2)
+                p1 = fill("I", L)
+                p1[Control_Qubit] = "PI_1"
+                p1[Target_Qubit] = "U"
+                H_k = Matrices[p1[1]]
+                for i = 2:L
+                    H_k = kron(H_k,Matrices[p1[i]])
+                end                                 
+            end
+    
+    
+        return f_k*H_k*(f_k')
+    end; 
+    
+    #= The following loop sums over all epsilon to get H_eff. =#
+    h_eff = zeros(2^L,2^L);
+    for i = 1:length(U_list)
+        h_eff += NOISE_list[i]*kth_term(i)
+    end        
+
+    #=
+    h_eff = DELTA * h_eff # Matrix in Z basis.
+    h_eff_D = (basis_change_matrix)*h_eff*(basis_change_matrix') # Matrix in |0> and |xbar> basis.
+
+    h_eff_D = h_eff_D[3:2^L,3:2^L]; # Deleting the |0> and |xbar> basis.
+    E_eff_D = eigvals(h_eff_D) # Diagonalizing H_eff matrix.
+    
+    E_eff_D_sorted = sort(real(E_eff_D),rev = true); # Soring the eigenvalues in descending order.    
+
+    
+    EIGU = py"eigu"(collect(GROVER_DELTA))
+    E_exact = real(1im*log.(EIGU[1])); # Eigenvalue.
+    E_exact = E_exact[2:2^L-1]; #= Neglecting the two special states at 1 and 2^L. =#    
+    
+    =#
+    #return E_exact, E_eff_D_sorted
+    return GROVER_DELTA, h_eff
+end;
+
+ket_0    = zeros(2^L)
+ket_0[1] = 1
+
+# Defining the state |x_bar> in sigma_z basis.
+N = 2^L
+ket_x    = (1/sqrt(N))   * ones(N)
+ket_xbar = sqrt(N/(N-1)) * ket_x - 1/sqrt(N-1)*ket_0 # Normalization checked.
+
+basis_1 = ket_0
+basis_2 = ket_xbar;
+
+DELTA = 0.0
+h_eff_matrix_whole = grover_effective_Hamiltonian_matrix(DELTA)[2]
+
+h_1_1 = basis_1' * h_eff_matrix_whole * basis_1
+h_1_2 = basis_1' * h_eff_matrix_whole * basis_2
+h_2_1 = basis_2' * h_eff_matrix_whole * basis_1
+h_2_2 = basis_2' * h_eff_matrix_whole * basis_2;
+
+
+#phi = atan(2*sqrt(N-1)/(2-N))
+# h_eff as 2x2 block matrix.
+##h_eff_block_matrix = pi*[1 0;0 1]+phi*[0 -1im; 1im 0] - DELTA*[ h_1_1 h_1_2; h_2_1 h_2_2]
+#h_eff_block_matrix_traceless = h_eff_block_matrix .- [1 0;0 1]*tr(h_eff_block_matrix)/2
+
+
+h_spec_elements_file  = open("h_spec_elements.txt", "w")
+write(h_spec_elements_file , string(real(h_1_1)))
+write(h_spec_elements_file, "\t")
+write(h_spec_elements_file , string(real(h_1_2)))
+write(h_spec_elements_file, "\t")
+write(h_spec_elements_file , string(real(h_2_1)))
+write(h_spec_elements_file, "\t")
+write(h_spec_elements_file , string(real(h_2_2)))
+close(h_spec_elements_file)
+
+
