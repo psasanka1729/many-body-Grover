1421078a4dd74a282c807e1893987c73f8504d69
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-09-18 23:33:28 -0500

    Commit before run h_eff_elements

--- build/h_eff_elements/h_eff_spec_elements.jl
+++ build/h_eff_elements/h_eff_spec_elements.jl
@@ -496,11 +496,11 @@ function h_eff_from_derivative(h)
 end;
 #h_eff_compt_basis = h_eff_from_derivative(1.e-6)
 h_eff_compt_basis = grover_effective_Hamiltonian_matrix(0.0)
-h_eff_compt_basis_no_trace = h_eff_compt_basis# - Identity(2^L)*(1/2^L)*tr(h_eff_compt_basis)
+#h_eff_compt_basis_no_trace = h_eff_compt_basis# - Identity(2^L)*(1/2^L)*tr(h_eff_compt_basis)
 #h_eff_0_xbar_basis = (basis_change_matrix)*G_delta_h_eff_matrix[2]*(basis_change_matrix')
-h_eff_0_xbar_basis = (basis_change_matrix)*h_eff_compt_basis_no_trace*(basis_change_matrix')
+#h_eff_0_xbar_basis = (basis_change_matrix)*h_eff_compt_basis_no_trace*(basis_change_matrix')
 
-save("h_eff_matrix.jld","h_eff",h_eff_0_xbar_basis)
+save("h_eff_matrix.jld","h_eff",h_eff_compt_basis)
 #=
 h_spec_elements_file  = open("h_spec_elements.txt", "w")
 write(h_spec_elements_file, string(real(h_eff_0_xbar_basis[1,1])))
