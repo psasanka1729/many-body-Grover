369a24939a215970bd1dfc745790c990283b389e
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-09-25 23:11:36 -0500

    Commit before run phi_delta

--- build/phi_delta/energy_noise.jl
+++ build/phi_delta/energy_noise.jl
@@ -1,11 +1,11 @@
-L = 14;
+L = 12;
 
 using Random
 using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
 
-file = raw"14_new_Grover_gates_data.txt" # Change for every L.
+file = raw"12_new_Grover_gates_data.txt" # Change for every L.
 M = readdlm(file)
 Gates_data_1 = M[:,1];
 Gates_data_2 = M[:,2];
@@ -29,7 +29,7 @@ Number_of_Gates = U_0_gate_number+U_x_gate_number
 
 
 # Good seeds = 10,14, 1945, 1337, 141421, 1414, 173205075, 1642, 1942.
-SEED = 4000
+SEED = 1945
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -534,10 +534,10 @@ def Write_file(Noise, Energy, Entropy):
 # delta_index runs from 0 to 128.
 delta_index = parse(Int64,ARGS[1])
 
-Delta = LinRange(0.0,0.2,64+1)
+Delta = LinRange(0.0,0.3,64+1)
 delta_start = Delta[delta_index+1]
 delta_end = Delta[delta_index+2]
-Num = 1
+Num = 4
 
 #=
 Arrays to hold delta, energy and entropy before they are written into the file.              
