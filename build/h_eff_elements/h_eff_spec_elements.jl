6075fff891f70bb33cc1dd5fb9f56fecb04ebb2b
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-10-01 15:55:11 -0500

    Commit before run h_eff_elements

--- build/h_eff_elements/h_eff_spec_elements.jl
+++ build/h_eff_elements/h_eff_spec_elements.jl
@@ -1,4 +1,4 @@
-L = 8;
+L = 12;
 
 using JLD
 using PyCall
@@ -497,8 +497,8 @@ function h_eff_from_derivative(h)
         h_eff_matrix = 1im*((Grover_delta(h)-Grover_delta(-h))/(2*h))*(-G_exact)'
         return h_eff_matrix
 end;
-h_eff_compt_basis = h_eff_from_derivative(1.e-6)
-#h_eff_compt_basis = grover_effective_Hamiltonian_matrix(0.0)
+#h_eff_compt_basis = h_eff_from_derivative(1.e-6)
+h_eff_compt_basis = grover_effective_Hamiltonian_matrix(0.0)
 #h_eff_compt_basis_no_trace = h_eff_compt_basis - Identity(2^L)*(1/2^L)*tr(h_eff_compt_basis)
 #h_eff_0_xbar_basis = (basis_change_matrix)*G_delta_h_eff_matrix[2]*(basis_change_matrix')
 #h_eff_0_xbar_basis = (basis_change_matrix)*h_eff_compt_basis_no_trace*(basis_change_matrix')
