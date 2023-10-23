d3895b27250e45a52c6473bf4231867b3e07ee19
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-10-23 14:37:34 -0500

    Commit before run probability_time

--- build/probability_time/probability_fit_data.jl
+++ build/probability_time/probability_fit_data.jl
@@ -242,7 +242,7 @@ xdata = [i for i = 50:70];
 ydata = p_0l[50:70]
 
 # Define an initial guess for the parameters
-p0 = [  0.2,   0.2,   0.19, 10]
+p0 = [  0.5,   0.5,   0.15, 10]
 
 # Call the curve_fit function
 fit = curve_fit(model, xdata, ydata, p0)
