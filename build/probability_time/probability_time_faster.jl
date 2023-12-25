e37794a360914e69206cb6ea812b3801ab9ade32
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2023-12-25 10:16:53 -0600

    Commit before run probability_time

--- build/probability_time/probability_time_faster.jl
+++ build/probability_time/probability_time_faster.jl
@@ -27,7 +27,7 @@ U_x_gate_number =  (L-1          # L-1 H gate on left of MCX
                   + L-1)          # L-1 X gate on right of MCX)             
 Number_of_Gates = U_0_gate_number+U_x_gate_number
 
-SEED = 100+parse(Int64,ARGS[1])
+SEED = 0+parse(Int64,ARGS[1])
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
@@ -186,7 +186,7 @@ end
 
 #delta_index = 1+parse(Int64,ARGS[1])
 #DELTAS = [0.0,0.01,0.02,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.15,0.2,0.21,0.24,0.3,0.4]
-Delta = 0.07#DELTAS[delta_index]
+Delta = 0.01#DELTAS[delta_index]
 
 G_delta = Grover_operator(Delta);
 
